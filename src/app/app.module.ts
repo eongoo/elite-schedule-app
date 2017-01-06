72d27d94b037559d39475e9ff792820a389d4724
eongoo <eongoo@qq.com>
2017-01-06 09:10:20 +0800

    22 - Navigation - Create Your Page Skeletons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { MyTeamsPage, TeamsPage, TeamDetailPage, TournamentsPage, GamePage} from '../pages/pages'
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    MyTeamsPage,
+    TeamsPage,
+    TeamDetailPage,
+    TournamentsPage,
+    GamePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +18,11 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    MyTeamsPage,
+    TeamsPage,
+    TeamDetailPage,
+    TournamentsPage,
+    GamePage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
